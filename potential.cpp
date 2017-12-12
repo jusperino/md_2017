f8b9bcb818e0aac784e6fe748d636dfe95bb9f43
lsc0498 <33378936+lsc0498@users.noreply.github.com>
2017-12-12 21:24:29 +0100

    Potential::distance_DIM gibt jetzt gerichtete Distanzen aus

--- potential.cpp
+++ potential.cpp
@@ -14,19 +14,20 @@ real Potential::distance_DIM(Particle &p, Particle &q, int dim){
 	// distance respecting periodic upper border, initialized with maximal distance since smallest calculated distance will be returned
 	real dist_upper = W.length[dim];
 	// distance without consideration of BorderType
-	real dist = std::abs(p.x[dim] - q.x[dim]);
+	real dist = p.x[dim] - q.x[dim];
 
 	// calculate dist_lower, dist_upper if BorderType is set "periodic"
 	if (W.lower_border[dim] == periodic){
 		dist_lower = std::min(p.x[dim], q.x[dim]) + W.length[dim] - std::max(p.x[dim], q.x[dim]);
 	}
 
+	//multiply with "-1" in order to get oriented distances
 	if (W.upper_border[dim] == periodic){
-		dist_upper = std::max(p.x[dim], q.x[dim]) + W.length[dim] - std::min(p.x[dim], q.x[dim]);
+		dist_upper = -(std::max(p.x[dim], q.x[dim]) + W.length[dim] - std::min(p.x[dim], q.x[dim]));
 	}
 
 	// return smallest calculated distance
-	return std::min({dist_lower, dist_upper, dist});
+	return std::min({std::abs(dist_lower), std::abs(dist_upper), std::abs(dist)});
 }
 
 real Potential::distance(Particle &p, Particle &q) {
