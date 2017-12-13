7123fb7eafff61f5f52608d3889e4b27f9347fa1
jusperino <jusperino@gmail.com>
2017-12-13 14:56:58 +0100

    added documentation to distance_DIM

--- potential.cpp
+++ potential.cpp
@@ -9,9 +9,12 @@ Potential::Potential(World& _W) : W(_W){
 }
 
 real Potential::distance_DIM(Particle &p, Particle &q, int i){
+	// if borders are nt periodic, simply return regular distance
 	if ((W.lower_border[i] != periodic) && (W.upper_border[i] != periodic)){
 		return q.x[i] - p.x[i];
 	} else {
+		// if one of the borders is not periodic, make sure that the distance calculated does not make use of a non-periodic border
+		// W.length is always greater than any other distance, so it will never win the following comparisons
 		if (W.lower_border[i] == periodic) {
 			real dist_lower = q.x[i] - W.length[i] - p.x[i];
 		} else {
@@ -26,6 +29,7 @@ real Potential::distance_DIM(Particle &p, Particle &q, int i){
 		
 		real dist = q.x[i] - p.x[i];
 
+		//deterine shortest of 3 options
         real shortest = W.length[i];
 		if (std::abs(dist_lower) <= std::abs(dist_upper)){
             shortest = dist_lower;
