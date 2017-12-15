511e0eb48f0e43c7b2ed2e6581e6cfb391c01b9a
Jasper <jusperino@gmail.com>
2017-12-15 16:41:22 +0100

    disabled double-checking border type (nonsensical input now results in nonsensical output)

--- potential.cpp
+++ potential.cpp
@@ -9,29 +9,17 @@ Potential::Potential(World& _W) : W(_W){
 }
 
 real Potential::distance_DIM(Particle &p, Particle &q, int i){
-	// if borders are nt periodic, simply return regular distance
-	if ((W.lower_border[i] != periodic) && (W.upper_border[i] != periodic)){
+	// if borders are not periodic, simply return regular distance
+	if (W.lower_border[i] != periodic){
 		return q.x[i] - p.x[i];
 	} else {
 		// if one of the borders is not periodic, make sure that the distance calculated does not make use of a non-periodic border
 		// W.length is always greater than any other distance, so it will never win the following comparisons
-		real dist_lower;
-		real dist_upper;
-		if (W.lower_border[i] == periodic) {
-			dist_lower = q.x[i] - W.length[i] - p.x[i];
-		} else {
-			dist_lower = W.length[i];
-		}
-
-		if (W.upper_border[i] == periodic) {
-			dist_upper = q.x[i] + W.length[i] - p.x[i];
-		} else {
-			dist_upper = W.length[i];
-		}
-		
+        real dist_lower = q.x[i] - W.length[i] - p.x[i];
+        real dist_upper = q.x[i] + W.length[i] - p.x[i];
 		real dist = q.x[i] - p.x[i];
 
-		//deterine shortest of 3 options
+		//determine shortest of 3 options
         real shortest = W.length[i];
 		if (std::abs(dist_lower) <= std::abs(dist_upper)){
             shortest = dist_lower;
@@ -47,7 +35,7 @@ real Potential::distance_DIM(Particle &p, Particle &q, int i){
 }
 
 real Potential::distance_2(Particle &p, Particle &q) {
-	//calculate euclidean norm of distance vector by looping over the dimensions
+	//calculate square of euclidean norm of distance vector by looping over the dimensions
 	real sqrsum = 0;
 	for (int i = 0; i<DIM; i++) {
 		real dist = distance_DIM(p, q, i);
