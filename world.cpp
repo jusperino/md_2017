24d410eac8d01e1fc60e232ca56a369e6cb346c2
jusperino <jusperino@gmail.com>
2018-01-09 16:43:50 +0100

    corrected determine_corr_cell

--- world.cpp
+++ world.cpp
@@ -241,8 +241,8 @@ int World::get_process_rank(std::vector<int> &j){
 }
 
 int World::determine_corr_cell(const Particle &p) {
-    // calculate cell coordinates
-    int J = std::min(std::floor(p.x[0]/cell_length[0]), cell_N[0]/S.N_p[0]-1.0);
+    // determine global sequential cell index that particle p is/should be located in
+    int J = std::min(std::floor(p.x[0]/cell_length[0]), cell_N[0]-1.0);
 	for (size_t i = 1; i < DIM; ++i){
 		int j = std::min(std::floor(p.x[i]/cell_length[i]), cell_N[i]-1.0);
 		J *= cell_N[i];
